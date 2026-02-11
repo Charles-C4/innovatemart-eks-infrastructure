fa200af59241d1a2acdbb140614ba77be937e711
Charles Chukwuleta <charleschukwuleta@gmail.com>
2026-02-11 13:52:23 +0100

    This is a simple change on the main.tf and an update to my pipeline

--- main.tf
+++ main.tf
@@ -24,9 +24,16 @@ provider "aws" {
   }
 }
 
+# Data source to handle authentication tokens for the pipeline
+data "aws_eks_cluster_auth" "main" {
+  name = aws_eks_cluster.main.name
+}
+
 provider "kubernetes" {
   host                   = aws_eks_cluster.main.endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.main.token
+
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.main.name]
@@ -38,6 +45,8 @@ provider "helm" {
   kubernetes {
     host                   = aws_eks_cluster.main.endpoint
     cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.main.token
+
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.main.name]
